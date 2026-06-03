1f55d3632de6eaf5ad7367c3f79558292b436f87
Virendra <rajputvirendra138@gmail.com>
2026-06-03 11:14:54 +0530

    updated the static input to parameter

--- Typed test data manager/src/services/userManager.ts
+++ Typed test data manager/src/services/userManager.ts
@@ -1,28 +1,32 @@
 // contains functions that work with users
 
 
-function addUser(){
-// add user
-console.log(users_list.length);
-}
-
-
-function getUserByEmail(){
-
-    // how to take an input here?
-
-const sample_email = "test.viru@gmail.com";
-let searchFound = false;
-for (let i=0; i< users_list.length;i++){
-    if (users_list[i].email == sample_email){
-        console.log("Found the email in the Users list");
-        searchFound = true;
-    }
-
-    if (!searchFound){
-        console.log("No email found.");
-    }
+function addUser() {
+    // add user
 
 }
 
+
+function getUserByEmail(emailInput: string) {
+    // let searchFound = false;
+    let userDetails: { name?: string; id?: string; role?:string; active?:boolean } = {};
+    if (users_list.length > 0) {
+        for (let i = 0; i < users_list.length; i++) {
+            if (users_list[i].email == emailInput) {
+                console.log("Found the email in the Users list");
+                // searchFound = true;
+                userDetails.name = users_list[i].name;
+                userDetails.role = users_list[i].role;
+                userDetails.active = users_list[i].active;
+                userDetails.id = users_list[i].id;
+            }
+        }
+    }
+    return userDetails;
+}
+
+function disaplyUsers() {
+    // this is total user count
+    console.log(users_list.length);
+    // display users one by one
 }
